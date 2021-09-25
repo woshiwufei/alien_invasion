6c77ef99c102437610826d0ca297c5e2ba354249
woshiwufei <1605299295@qq.com>
2021-09-25 17:05:46 +0800

    重构： 方法_check_events() 和 __update_screen()

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,17 +23,22 @@ class AlienInvasion:
         """开始游戏的主循环"""
 
         while True:
-            # 监视键盘和鼠标事件、
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            # 每次循环时都重绘屏幕
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+    def _check_events(self):
+        """响应按键和鼠标事件"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            # 让最近绘制的屏幕可见
-            pygame.display.flip()
+    def _update_screen(self):
+        """更新屏幕上的图像，并切换到新屏幕"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # 让最近绘制的屏幕可见
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
