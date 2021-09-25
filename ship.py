9aef7d5a70842c33e3085316224c3f8dc007f01a
woshiwufei <1605299295@qq.com>
2021-09-25 16:58:21 +0800

    改正少了screen错误

--- ship.py
+++ ship.py
@@ -7,7 +7,7 @@ class Ship:
 	def __init__(self, ai_game):
 		"""初始化飞船并设置其初始位置"""
 		self.screen = ai_game.screen
-		self.screen_rect = ai_game.get_rect()
+		self.screen_rect = ai_game.screen.get_rect()
 
 		# 加载飞船图像并获取其外接矩形
 		self.image = pygame.image.load('images/ship.bmp')
