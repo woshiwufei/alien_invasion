dc7f45ee8b3a6dcb29d3f7a0ac9aac4584e738cb
woshiwufei <1605299295@qq.com>
2021-09-25 16:47:28 +0800

    添加飞船图像，创建Ship 类

---
+++ ship.py
@@ -0,0 +1,20 @@
+import pygame
+
+
+class Ship:
+	"""管理飞船的类"""
+
+	def __init__(self, ai_game):
+		"""初始化飞船并设置其初始位置"""
+		sef.screen = ai_game.screen
+		self.screen_rect = ai.game.get_rect()
+
+		# 加载飞船图像并获取其外接矩形
+		self.image = pygame.image.load('images/ship.bmp')
+		self.rect = self.image.get_rect()
+
+		# 对于每艘新飞船，都将其放在屏幕底部的中央。
+		self.rect.midbottom = self.screen_rect.midbottom
+
+	def blitme(self):
+		self.screen.blit(self.image, self.rect)
