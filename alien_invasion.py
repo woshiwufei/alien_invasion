34335b5dddb2feeb4d70b191cffe21c27fcafa97
woshiwufei <1605299295@qq.com>
2021-09-26 16:20:32 +0800

    修改为全屏游戏

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,7 +14,9 @@ class AlienInvasion:
         self.settings = Settings()
 
         self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+            (0, 0), pygame.FULLSCREEN)
+        self.screen_width = self.screen.get_rect().width
+        self.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
