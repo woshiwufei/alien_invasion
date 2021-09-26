5b711ba95f3302ebdd0aedd7a87d4b3781806368
woshiwufei <1605299295@qq.com>
2021-09-26 10:56:57 +0800

    按Q键退出

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,18 +33,27 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    # 向右移动飞船
-                    self.ship.moving_right =True
-                elif event.key == pygame.K_LEFT:
-                    # 向左移动飞船
-                    self.ship.moving_left =True
-                    
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """响应按键"""
+        if event.key == pygame.K_RIGHT:
+            # 向右移动飞船
+            self.ship.moving_right =True
+        elif event.key == pygame.K_LEFT:
+            # 向左移动飞船
+            self.ship.moving_left =True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """响应松开按键"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕"""
