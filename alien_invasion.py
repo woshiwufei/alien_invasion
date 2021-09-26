e51161789f6a14799b3c43cf60e97e78d70e4c5b
woshiwufei <1605299295@qq.com>
2021-09-27 02:01:45 +0800

    重构主循环代码，抽取出更新子弹位置和删除消失子弹的代码放在_update_bullets方法中

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,13 +29,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-
-            # 删除消失的子弹
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
@@ -73,6 +67,16 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """更新子弹的位置并删除消失的子弹"""
+        # 更新子弹位置
+        self.bullets.update()
+
+         # 删除消失的子弹
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕"""
