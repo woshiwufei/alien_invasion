0bbd354d9b5687cb702ea3da4424b58e4f3dbbf2
woshiwufei <1605299295@qq.com>
2021-09-27 08:27:58 +0800

    创建一群外星人

--- alien_invasion.py
+++ alien_invasion.py
@@ -87,16 +87,29 @@ class AlienInvasion:
         # 创建一个外星人并计算一行可以容纳的个数
         # 外星人的间距为一个外星人宽度
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - 2 * alien_width
         number_aliens_x = available_space_x // (2 * alien_width) 
 
-        # 创建第一行外星人
-        for alien_number in range(number_aliens_x):
-            alien = Alien(self)
-            alien.x = alien_width + 2 * alien_width * alien_number
-            alien.rect.x = alien.x
-            self.aliens.add(alien)
+        # 计算屏幕可容纳多少行外星人
+        ship_height = self.ship.rect.height
+        available_space_y = self.settings.screen_height - 3 * alien_height - ship_height
+        number_rows = available_space_y // (2 * alien_height)
+
+        # 创建外星人群
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        """创建一个外星人并放在当前行"""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.y = alien.rect.height + 2 * alien.rect.height * row_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.y
+        self.aliens.add(alien)
 
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕"""
